9244de2a83218fb4f0d4155a601aa5131a0a3c72
Jacob Paltrowitz <jpaltrowitz30@stuy.edu>
2022-02-18 11:47:57 -0500

    maze solve

--- 03-maze/Maze.java
+++ 03-maze/Maze.java
@@ -1,3 +1,37 @@
 public class Maze{
+  private char[][] maze;
 
+  public static String toString(){
+    for (int i = 0; i < maze.length; i++){
+      for (int j = 0; j < maze[i].length; j++){
+        if (maze[i][j]=='#'){
+          System.out.print("#");
+        }else if(maze[i][j]=='X'){
+          System.out.print(Text.colorize("+", Text.GREEN));
+        }else{
+          System.out.print(" ");
+        }
+      }
+    }
+  }
+  public static boolean solve(){
+    for (int i = 0; i < maze.length; i++){
+      for (int j = 0; j < maze[i].length(); j++){
+        if (maze[i][j] == 'S'){
+          return solve(maze, 0, 0, i, j);
+        }
+      }
+    }
+  }
+
+  public static boolean solve(int row, int col){
+    if(maze[row][col] == '#' || maze[row][col] == 'X'){
+      return false;
+    }
+    else{
+      maze[row][col] = 'X';
+      return solve(maze, row+1, col)||solve(maze, row, col+1)||solve(maze, row-1, col)||solve(maze, row, col-1);
+
+    }
+  }
 }
