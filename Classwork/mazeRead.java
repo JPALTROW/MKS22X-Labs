71c38f8e515b2223e46e4c23c7889aa1d1936045
Jacob Paltrowitz <jpaltrowitz30@stuy.edu>
2022-02-18 11:25:24 -0500

    readMaze

--- Classwork/mazeRead.java
+++ Classwork/mazeRead.java
@@ -14,4 +14,32 @@ public class mazeRead{
       System.exit(0);
     }
   }
+
+  public static char[][] readMaze(String filename){
+    ArrayList<String[]> maze = new ArrayList<String[]>();
+    String[] line;
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      while (input.hasNextLine()){
+        line = input.nextLine().split("");
+        maze.add(line);
+      }
+    }catch (FileNotFoundException ex){
+      System.out.println("No such file");
+      System.exit(0);
+    }
+    char[][] compiled = new char[maze.size()][maze.get(1).length];
+    for (int i = 0; i < maze.size(); i++){
+      for (int j = 0; j < maze.get(i).length; j++){
+        compiled[i][j] = maze.get(i)[j].charAt(0);
+      }
+    }
+    return compiled;
+  }
+
+  public static void main(String[] args) {
+    printMaze("Maze1.txt");
+    System.out.println(Arrays.deepToString(readMaze("Maze1.txt")));
+  }
 }
