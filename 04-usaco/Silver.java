bc3627cda2f2fe1135df12d8842a014fc223df32
Jacob Paltrowitz <jpaltrowitz30@stuy.edu>
2022-03-08 14:03:17 -0500

    fixed type issue

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -1,6 +1,5 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Silver{
   public static long solve(String fileName){
@@ -15,11 +14,18 @@ public class Silver{
       char[][] field = new char[N][M];
       for (int i = 0; i < N; i++){
         line = input.nextLine();
-        data = line.split(" ");
+        data = line.split("");
         for (int j = 0; j < M; j++){
           field[i][j] = data[j].charAt(0);
         }
       }
+      line = input.nextLine();
+      data = line.split(" ");
+      int r1 = Integer.parseInt(data[0]);
+      int c1 = Integer.parseInt(data[1]);
+      int r2 = Integer.parseInt(data[2]);
+      int c2 = Integer.parseInt(data[3]);
+      return count(field, r1, c1, r2, c2, T, 0);
     }catch (FileNotFoundException ex){
       System.out.println("No such file");
       System.exit(0);
@@ -29,25 +35,29 @@ public class Silver{
 
   public static long count(char[][] field, int r1, int c1, int r2, int c2, int T, long count){
     int[][] dirs = {{0,1}, {0, -1}, {1,0}, {-1, 0}};
-    int counter = 0;
+    long counter = 0;
     if (field[r1][c1] == '*'){
-      return 0;
+      return 0l;
     }
     if (r1 < 0 || c1< 0 || r1 >= field.length||c1 >= field[0].length){
-      return 0;
+      return 0l;
     }
     if (Math.abs(r1-r2)+Math.abs(c1-c2) > T){
-      return 0;
+      return 0l;
     }
     if (T == 0){
       if(r1 == r2 && c1==c2){
-        return 1;
+        return 1l;
       }
-      return 0;
+      return 0l;
     }
     for (int i = 0; i < 4; i++){
       counter += count(field, r1+dirs[i][0], c1+dirs[i][1], r2, c2, T-1, count);
     }
     return counter;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(solve("tester2.txt"));
   }
 }
