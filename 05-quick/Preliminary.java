f2160217d9f1da2ea49c4caa9a9ea8af0d4b6a04
Jacob Paltrowitz <jpaltrowitz30@stuy.edu>
2022-03-15 11:36:09 -0400

    tested

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Preliminary{
   /*Modify the array such that:
   *1. A random index from start to end inclusive is chosen, the
@@ -12,26 +14,44 @@ public class Preliminary{
   public static int partition ( int [] data, int start, int end){
     int pivot = (int)(Math.random() * (end-start+1))+start;
     int store = data[pivot];
-    data[pivot]= data[start];
+    data[pivot] = data[start];
     boolean lastEq = true;
     pivot = start;
     data[start] = store;
     int hold;
+    int pivotP = 0;
+
+
     for(int i = start+1; i <= end; i++){
       hold = data[i];
       if (hold == store){
         lastEq = !lastEq;
       }
       if (hold < store || (lastEq && hold == store)){
-        data[i]=store;
-        data[pivot] = hold;
-        pivot = i;
+        pivotP++;
+
       }else if(hold > store || (lastEq && hold == store)){
         data[i]=data[end];
         data[end] = hold;
         end--;
+        i--;
       }
+
     }
-    return 0;
+    data[0]=data[pivotP];
+    data[pivotP]=store;
+
+    return pivotP;
+  }
+
+  public static void main(String[] args) {
+    //int [] data = new int[] {4,3,2,1,0};
+    int[] data = new int[] {0,0,0,0,0,0,0,0,0};
+    System.out.println("Original: "+Arrays.toString(data));
+    int pivot = partition( data , 0, 7);
+    System.out.println("Pivot value: "+data[pivot]+ ", Pivot index: "+pivot);
+    System.out.println("Modified: "+Arrays.toString(data));
+    System.out.println();
+
   }
 }
