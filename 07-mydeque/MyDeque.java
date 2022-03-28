98624753a61484ca7d48fc22006c2e55d703d15e
Jacob Paltrowitz <jpaltrowitz30@stuy.edu>
2022-03-28 11:17:45 -0400

    Constructors

---
+++ 07-mydeque/MyDeque.java
@@ -0,0 +1,26 @@
+public class MyDeque<E>{
+  private E[] data;
+  private int size;
+  private int start, end;
+
+
+  public MyDeque(int initialCapacity){
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[initialCapacity];
+    data = d;
+  }
+  public MyDeque(){
+    this(10);
+  }
+
+  public int size(){
+    return size;
+  }
+  // public String toString(){ }
+  // public void addFirst(E element){ }
+  // public void addLast(E element){ }
+  // public E removeFirst(){ }
+  // public E removeLast(){ }
+  // public E getFirst(){ }
+  // public E getLast(){ }
+}
