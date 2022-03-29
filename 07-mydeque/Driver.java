e95900cc228eebe24eab0509b4a9e5854ca77592
Jacob Paltrowitz <jpaltrowitz30@stuy.edu>
2022-03-29 11:20:50 -0400

    testing

--- 07-mydeque/Driver.java
+++ 07-mydeque/Driver.java
@@ -5,5 +5,10 @@ public class Driver{
     tester.addFirst(1345);
     tester.addLast(987654);
     System.out.println(tester);
+    tester.removeLast();
+    tester.addLast(1);
+    tester.addLast(123456);
+    tester.removeFirst();
+    System.out.println(tester);
   }
 }
