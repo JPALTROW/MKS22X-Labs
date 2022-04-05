6a419ee1507a5e74272c5995cd2ed2c7f0c70c0d
Jacob Paltrowitz <jpaltrowitz30@stuy.edu>
2022-04-05 11:23:46 -0400

    09 Base Code

---
+++ 09-treeburning/Frontier.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Frontier{
+  private ArrayDeque<int[]> frontier;
+  public Frontier(){}
+  public int size(){
+    return 0;
+  }
+  public void add(int[]location){}
+  public int[] remove(){
+    return new int[0];
+  }
+}
